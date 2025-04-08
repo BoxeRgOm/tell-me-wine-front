658cfdfae2468f50f1f80fe5e8759bdb3defabaa
boxergom <artipact@naver.com>
2025-04-08 18:45:50 +0900

    chage ai model button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import ChatBot from "./pages/chatbot";
-import {Routes, Route } from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
-
   return (
     <Routes>
-      <Route path="/" element={<ChatBot type={"openai"}/>}/>
-      <Route path="/deepseek" element={<ChatBot type={"deepseek"}/>}/>
+      <Route path="/" element={<ChatBot/>}/>
     </Routes>
   ) 
 }
--- src/pages/chatbot/index.js
+++ src/pages/chatbot/index.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import axios from 'axios';
 
 import {
@@ -13,17 +13,24 @@ import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import { api_url } from '../../modules/constants';
 
 
-const ChatBot = ({type}) => {
+const ChatBot = () => {
+    const [type, setType] = useState('openai') // 'openai' || 'deepseek'
 
     const [messageList, setMessageList] = useState([
-        { 
-            message: `Model : ${type}`,
-            direction: 'incoming',
-            position: 'first'},
-        { 
-        message: '안녕하세요. Wine Chat Bot "Tell Me Wine." 입니다^^',
-        direction: 'incoming',
-        position: 'first'}])
+        { message: '안녕하세요. Wine Chat Bot "Tell Me Wine." 입니다^^',
+          direction: 'incoming',
+          position: 'first'},
+        { message: `AI Model : ${type}`,
+          direction: 'incoming',
+          position: 'normal'}])
+
+    useEffect(()=>{
+        setMessageList([...messageList,
+            { message: `Change AI Model : ${type}`,
+              direction: 'incoming',
+              position: 'normal'}
+        ])
+    },[type, setType])
 
     const sendCommened = async (text) => {
 
@@ -56,6 +63,18 @@ const ChatBot = ({type}) => {
 
     return (<>
         <h3>ChatBot</h3>
+        <div>
+            <button onClick={()=>{
+                if(type==='deepseek'){
+                    setType('openai')
+                }
+            }}>Open AI</button>
+            <button onClick={()=>{
+                if(type==='openai'){
+                    setType('deepseek')
+                }
+            }}>DeepSeek</button>
+        </div>
          <MainContainer style={{ 
                 width: '100vw', 
                 height: 360,
