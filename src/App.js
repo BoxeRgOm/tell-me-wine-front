591ed195f2fcacc7597061352093117f6050e342
boxergom <artipact@naver.com>
2025-04-08 16:26:01 +0900

    배포하면 라우팅 안돼지?

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 import ChatBot from "./pages/chatbot";
-import Admin from "./pages/admin";
-import Feedback from "./pages/feedback";
-
-import { Routes, Route, Link } from "react-router-dom";
-
-import { useEffect } from "react";
 
 function App() {
+
   return (
-    <Routes>
-      <Route path="/" element={<ChatBot />} />
-      <Route path="/admin" element={<Admin />} />
-      <Route path="/feedback" element={<Feedback />} />
-    </Routes>
-
-  );
-
-  // useEffect(()=>{
-  //   console.log('App')
-  // },[])
-
-  // return (
-  //   <>
-  //     <ChatBot />
-  //   </>
-  // ) 
+    <>
+      <ChatBot />
+    </>
+  ) 
 }
 
 
